5e6e069b0ea28ab7d22ad6b221a4f97addee9b70
MTaurus <97911761+mau1878@users.noreply.github.com>
2024-09-25 17:33:51 -0300

    Update acc-vs-infla.py

--- acc-vs-infla.py
+++ acc-vs-infla.py
@@ -139,7 +139,7 @@ def generar_grafico(expression_str, df, cumulative_inflation, year=None, date_ra
       # Crear la figura
       fig = go.Figure()
       fig.add_trace(go.Scatter(x=df['Date'].iloc[:min_length], y=df['Result'].iloc[:min_length],
-                               name="Activo o cartera seleccionada", mode='lines'))
+                               name="Activo", mode='lines'))
       fig.add_trace(go.Scatter(x=df['Date'].iloc[:min_length], y=inflation_line,
                                name='Inflación', line=dict(dash='dash', color='red'), mode='lines'))
 
